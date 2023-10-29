3074db4b48a0e14f309d79761ca8f029bcd7eb07
redhotchillipuppers <johnconnolly38@googlemail.com>
2023-10-29 01:47:13 +0100

    Added functionality to contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,91 +1,35 @@
-import React from "react";
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import '../App.css';
-import { useState } from "react";
 
-function Contact () {
-    
-    const [name, setName] = useState('');
-    const [company, setCompany] = useState('');
-    const [tel, setTel] = useState('');
-    const [email, setEmail] = useState('');
-    const [message, setMessage] = useState('');
+const Contact = () => {
+  const form = useRef();
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-    };
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-    return (
-        <div className='contact'>
-    <h1>Contact Me:</h1>
-    <h3>I am open to Employment, Contract, and Freelance work.</h3>
-    <div className='contactBox'>
-        <h3>Please submit the form below and I will contact you</h3>
-        <form onSubmit={handleSubmit}>
-        <p>Name
-            <input 
-                type='text' 
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-            />
-        </p>
-        <p>Company
-            <input 
-            maxLength='19' 
-            value={company}
-            onChange={(e) => setCompany(e.target.value)}
-            />
-        </p>
-        <p>Tel
-            <input 
-            type='text' 
-            name='number' 
-            maxLength='11' 
-            value={tel}
-            onChange={(e) => setTel(e.target.value)}
-            />
-        </p>
-        <p>Email
-            <input 
-            type='email' 
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            />
-        </p>
-        <p>Message
-            <input 
-            maxLength='200' 
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            />
-        </p>
-        <p>Preferred contact method</p>
-        <span>
-        <label>
-            Phone
-            <input type='radio' 
-            value='Phone'
-            />
-        </label>
-        <label>
-            E-mail
-            <input type='radio' 
-            />
-        </label>
-        <label>
-            Other
-            <input 
-            type='radio' 
-            />
-            </label><br/>
-            <button 
-            type='submit' 
-            className='sub'
-            >Submit</button>
-        </span>
-        </form>
-    </div>
-    </div>
-    )
+    emailjs.sendForm('service_zagzslo', 'template_4a4y2h7', form.current, 'my975geqjzB4cGzNC')
+      .then((result) => {
+          console.log(result.text);
+          alert('Thank you, I will be in touch')
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Phone Number</label>
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  );
 };
+
 
 export default Contact;
